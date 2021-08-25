141fa25f835fd21c5bb51eb369035ec9b049d4bb
Chen-kunX <71976753+Chen-kunX@users.noreply.github.com>
2021-08-25 18:25:00 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,18 @@ let myHeading = document.querySelector('h2');
 function setUserName() {
     let myName = prompt('输入你的名字：');
     if(myName == null || !myName){
+        alert('名字不许为空哦！');
         setUserName();
     }else {
     localStorage.setItem('name',myName);
-    myHeading.textContent = '666 my bro:' + myName;
+    myHeading.textContent = 'Hello!' + myName;
     }
 }
 if(!localStorage.getItem('name')) {
     setUserName();
-} else {
+} else { 
     let storedName = localStorage.getItem('name');
-    myHeading.textContent = '666 my bro: ' + storedName;
+    myHeading.textContent = 'I love you ' + storedName;
 }
 myButton.onclick = function() {
     setUserName();
